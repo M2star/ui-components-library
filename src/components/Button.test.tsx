eed97d363645d607a2288d86f0a3df65a54fd9f2
m2star <tushar.bawane23@gmail.com>
2024-01-03 12:17:19 +0530

    add story book

--- src/components/Button.test.tsx
+++ src/components/Button.test.tsx
@@ -1,10 +1,9 @@
-import React from "react";
-import { render } from "@testing-library/react";
+import React from 'react';
+import { render } from '@testing-library/react';
+import Button from './Button';
 
-import Button from "./Button";
-
-describe('Button', () => 1{
-    test('render the button component', () => {
-        render(: any<Button: any>hello world</Button:>)
+describe('Button', () => {
+    test('renders the button component', () => {
+        render(<Button>Hello World</Button>);
     });
-})
+});
